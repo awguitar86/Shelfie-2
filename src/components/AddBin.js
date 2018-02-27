61691cfdbc1c7a4959357d75c5ee03440b029b6e
Austin Wright <awguitar86@gmail.com>
2018-02-26 18:27:44 -0700

    created new components and imported React and Link

---
+++ src/components/AddBin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { postBin } from '../services/api.services';
+
+class AddBin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            price: '0',
+        };
+
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
-
+import AddBin from './AddBin';
+import Bin from './Bin';
+import BinList from './BinList';
+import ShelfList from './ShelfList';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Route exact path='/' component={ShelfList}   />
+        <Route path='/shelf/:id/' component={BinList} />
+        <Route path='/create/:id' component={AddBin}  />
+        <Route path='/bin/:id' component={Bin}        />
       </div>
     );
   }
